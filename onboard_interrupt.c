cd27f615d7b6657f1a7150877dd3ee145bec101d
maddalikaushik <105742556+maddalikaushik@users.noreply.github.com>
2022-06-14 22:55:19 +0530

    Create onboard_interrupt.c

---
+++ onboard_interrupt.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "NUC1xx.h"
+#include "Driver\DrvSYS.h"
+#include "Driver\DrvGPIO.h"
+
+void EINT1Callback(void)
+{
+    DrvGPIO_ClrBit(E_GPB,11);
+    DrvSYS_Delay(300000);
+    DrvGPIO_SetBit(E_GPB,11);
+    DrvSYS_Delay(300000);
+    DrvGPIO_ClrBit(E_GPB,11);
+    DrvSYS_Delay(300000);
+    DrvGPIO_SetBit(E_GPB,11);
+}
+
+int main (void)
+{
+    UNLOCKREG();
+    DrvSYS_SetOscCtrl(E_SYS_XTL12M, 1); 
+    DrvSYS_SelectHCLKSource(0);         
+    LOCKREG();
+    DrvGPIO_Open(E_GPB, 11, E_IO_OUTPUT);
+    DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);                            
+    DrvGPIO_EnableEINT1(E_IO_BOTH_EDGE, E_MODE_EDGE, EINT1Callback); 
+    while(1)
+    {
+    }
+}
+
